2b5e84c364430002d715897b44ca87c346ada6ed
Alaet <72195619+Alaet@users.noreply.github.com>
2022-04-16 10:45:17 +0200

    lint update

--- lettings/tests/unit_tests/test_index.py
+++ lettings/tests/unit_tests/test_index.py
@@ -1,12 +1,9 @@
 import pytest
 from django.urls import reverse
 
-from lettings.models import Address, Letting
-
 
 @pytest.mark.django_db()
 class TestIndex:
-
     def test_index(self, client):
         url = reverse("lettings_index")
         response = client.get(url)
@@ -14,4 +11,4 @@ class TestIndex:
         # data = response.data.decode()
         expected_value = "Joshua Tree Green Haus /w Hot Tub"
         assert response.status_code == 200
-        assert expected_value in response.content.decode('utf-8')
+        assert expected_value in response.content.decode("utf-8")
--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -2,8 +2,7 @@ from django.urls import reverse
 
 
 class TestHome:
-
     def test_home(self, client):
-        url = reverse('index')
+        url = reverse("index")
         rv = client.get(url)
         assert rv.status_code == 200
