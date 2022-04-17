c0e5596e03654864caa7973666786f185dddc3d0
Alaet <72195619+Alaet@users.noreply.github.com>
2022-04-17 22:45:35 +0200

    additional env sett
    added local env conf

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -7,6 +7,7 @@ import environ
 # Initialize environment variables
 env = environ.Env()
 environ.Env.read_env()
+# environ.Env.read_env(env.str('.', '.env'), overwrite=True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
