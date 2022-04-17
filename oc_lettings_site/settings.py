8593ada565ceee6d4729c5033c6984aef681e33f
Alaet <72195619+Alaet@users.noreply.github.com>
2022-04-17 11:03:11 +0200

    fix settings
    installed django-heroku package for setup settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 from typing import List
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -109,10 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = "/static/"
-
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = 'staticfiles/'
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
