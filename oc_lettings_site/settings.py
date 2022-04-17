28f0ba2c71fb3c1061363aeb459f29828f970def
Alaet <72195619+Alaet@users.noreply.github.com>
2022-04-17 11:25:43 +0200

    fixed settings with env
    add environ package to setup env vairables

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,6 +1,11 @@
 import os
+import environ
 import django_heroku
 from typing import List
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -10,12 +15,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS: List[str] = []
+ALLOWED_HOSTS: List[str] = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
