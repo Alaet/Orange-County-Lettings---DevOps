cc8deb1ece82a8b77710477b61a854593d24bcfe
Alaet <72195619+Alaet@users.noreply.github.com>
2022-04-20 11:09:54 +0200

    refract lint

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,9 +1,10 @@
 import os
 from typing import List
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+
 import django_heroku
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Initialize environment variables
 env = environ.Env()
@@ -129,16 +130,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 sentry_sdk.init(
-    dsn=env('SENTRY_DSN'),
+    dsn=env("SENTRY_DSN"),
     integrations=[DjangoIntegration()],
-
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
     traces_sample_rate=1.0,
-
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
+    send_default_pii=True,
 )
 django_heroku.settings(locals())
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 
 
-def trigger_error(request):
+"""def trigger_error(request):
     division_by_zero = 1 / 0
-
+"""
 
 urlpatterns = [
     path("", include("lettings_site.urls")),
     path("", include("lettings.urls")),
     path("", include("profiles.urls")),
     path("admin/", admin.site.urls),
-    path("sentry-debug/", trigger_error),
+    # path("sentry-debug/", trigger_error),
 ]
